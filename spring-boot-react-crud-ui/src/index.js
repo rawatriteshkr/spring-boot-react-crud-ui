1c92b25a213d520c5b7200585a0693df203099a8
admin <admin@DESKTOP-82POU4M>
2021-02-03 08:43:23 +0530

    import bootstrap.min.css to index.js file for better ui

--- spring-boot-react-crud-ui/src/index.js
+++ spring-boot-react-crud-ui/src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
